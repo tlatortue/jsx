188880d3701fbf6ce246e2e531c000ef501dcab3
Thomas Latortue <thomaslatortue@Thomass-MBP.frontierlocal.net>
2019-05-15 21:14:22 -0400

    use props

--- src/index.js
+++ src/index.js
@@ -1,15 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import faker from 'faker';
 import CommentDetail from './CommentDetail';
+import ApprovalCard from './ApprovalCard';
 
 const App = () => {
     return (
         <div className="ui container comments">
-            <CommentDetail />
-            <CommentDetail />
-            <CommentDetail />
-            <CommentDetail />
-            <CommentDetail />
+            <ApprovalCard>
+                <CommentDetail author="Sam" timeAgo="Today at 4:45PM" content="Nice blog post" avatar={faker.image.avatar()}/>
+            </ApprovalCard>
+            <CommentDetail author="Alex" timeAgo="Today at 2:00AM" content="Blah blah" avatar={faker.image.avatar()}/>
+            <CommentDetail author="Jane" timeAgo="Yesterday at 6:15PM" content="foo" avatar={faker.image.avatar()}/>
         </div>
     );
 }
